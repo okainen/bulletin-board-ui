85878a1de70cdb6395249482afa931d9fdeed61d
okainen <vitalii.popovych.1@gmail.com>
2020-12-03 11:12:48 +0200

    Added express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { join } = require('path');
+
+
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(express.static(join(__dirname, '.dist')));
+
+app.get('/', (_, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
+});
+
+app.listen(port);
