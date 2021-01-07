a625f519e730c3ab52f99c99a8211f18d9911ebf
okainen <vitalii.popovych.1@gmail.com>
2021-01-07 08:55:54 +0200

    Made GET / route catch-all to make client-side routing work properly

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const { join } = require('path');
-
+const {join} = require('path');
 
 const port = process.env.PORT || 8080;
 const app = express();
+
 app.use(express.static(join(__dirname, '.dist')));
 
-app.get('/', (_, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+app.get('/*', (_, res) => {
+  res.sendFile(join(__dirname, '.dist/index.html'));
 });
 
 app.listen(port);
