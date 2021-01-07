02cb93d13cf064cca242ae658cb844fb3d9fb863
okainen <vitalii.popovych.1@gmail.com>
2021-01-07 08:49:20 +0200

    Updated all packages & added dependency on env vars & removed gh-pages deploy

--- webpack.common.js
+++ webpack.common.js
@@ -1,51 +1,55 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const jsPath = './src';
-const srcPath = path.join(__dirname, jsPath);
-const distPath = './.dist';
-const outputPath = path.join(__dirname, distPath);
+const srcPath = path.join(__dirname, 'src');
+const distPath = path.join(__dirname, '.dist');
+module.exports.distPath = distPath;
 
-module.exports = {
-    entry: {
-        test: [path.join(srcPath, '/index.js')]
-    },
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Production',
-        template: 'index.html',
-        filename: 'index.html'
-    })],
-    output: {
-        filename: 'bundle.js',
-        path: outputPath
-    },
-    context: srcPath,
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['babel-loader']
-            },
-            {
-                test: /\.worker\.js$/,
-                use: { loader: 'worker-loader' }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)$/,
-                loader: 'url-loader?limit=8192'
-            }
-        ]
-    }
+const isDev = process.env.NODE_ENV == 'development';
+
+module.exports.commonConf = {
+  entry: {
+    test: [path.join(srcPath, '/index.js')]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      filename: 'index.html'
+    }),
+    new webpack.EnvironmentPlugin(['NODE_ENV', 'AUTH_HOST', 'POSTS_HOST'])
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: distPath
+  },
+  context: srcPath,
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.worker\.js$/,
+        use: {loader: 'worker-loader'}
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192
+        }
+      }
+    ]
+  }
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,25 +1,22 @@
 const path = require('path');
-const { merge } = require('webpack-merge');
+const {merge} = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 const distPath = './dist';
 const outputPath = path.join(__dirname, distPath);
 
 module.exports = merge(common, {
-    mode: 'development',
-    watchOptions: {
-        ignored: '/node_modules/',
-        aggregateTimeout: 300,
-        poll: 1000
-    },
-    devServer: {
-        contentBase: outputPath,
-        compress: true,
-        port: 3000,
-        open: true,
-        historyApiFallback: true,
-        proxy: {
-            '/api': 'http://localhost:9000'
-        }
-    },
+  mode: 'development',
+  watchOptions: {
+    ignored: '/node_modules/',
+    aggregateTimeout: 300,
+    poll: 1000
+  },
+  devServer: {
+    contentBase: outputPath,
+    compress: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true
+  }
 });
